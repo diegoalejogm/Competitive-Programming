50cb312bf4cc7cbbbfe7c0abea0a78fe62864a60
Diego Gomez <diegoalejogm@icloud.com>
2017-01-12 00:24:25 -0500

    fix: corrected ReducedString and simplified it.

--- HackerRank/strings/ReducedString.java
+++ HackerRank/strings/ReducedString.java
@@ -7,35 +7,33 @@ public class ReducedString{// Solution {
         Scanner sc = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
 
-        String s = sc.nextLine();
-        if(s.equals("") || s.length() == 1){ pw.println(s); pw.close(); return;}
+        String s = sc.nextLine(), empty = "Empty String";
+        if(s.equals("")){pw.println(empty); pw.close(); return;}
+        if(s.length() == 1){ pw.println(s); pw.close(); return;}
 
         StringBuilder red = new StringBuilder();
-        Character current = s.charAt(0); int count = 1;
+        Character current = s.charAt(0); long count = 1;
 
         for(int i = 1; i < s.length() ; i++)
         {
           Character c = s.charAt(i);
-          if(c.equals(current)){ count++; }
+          if(c.equals(current)){ count++;}
           else{
-            Character last = null;
-            if(red.length()>0) {last = red.charAt(red.length()-1);};
-            if(count%2==1){
-              if(last == null || last.equals(current)){red.append(current);}
-              else{red.deleteCharAt(red.length()-1);}
-            }
+            addRemoveChar(red, count, current);
             count = 1; current = c;
           }
-
-          Character last = null;
-          if(red.length()>0) {last = red.charAt(red.length()-1);};
-          if(count%2==1){
-            if(last == null || last.equals(current)){red.append(current);}
-            else{red.deleteCharAt(red.length()-1);}
-          }
-
-          pw.println(red.toString());
-          pw.close();
         }
+        addRemoveChar(red, count, current);
+
+        pw.println(red.length()==0?empty:red.toString()); pw.close();
+    }
+
+    public static void addRemoveChar(StringBuilder sb, long count, char current){
+      Character last = null;
+      if(sb.length()>0) {last = sb.charAt(sb.length()-1);};
+      if(count%2==1){
+        if(last != null && last.equals(current)){sb.deleteCharAt(sb.length()-1);}
+        else{sb.append(current);}
+      }
     }
 }
